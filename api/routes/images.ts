a197ea158b0df8afa94dd1a110cf7848cd636e6c
leontomer <41841316+leontomer@users.noreply.github.com>
2023-05-16 14:19:00 +0300

    added sort and paginate routes

--- api/routes/images.ts
+++ api/routes/images.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response, Router } from "express";
 const router: Router = express.Router();
 import axios, { AxiosResponse } from "axios";
+import { Image } from "../../client/src/Types";
 
 router.get("/images", async (req: Request, res: Response) => {
   try {
     // Make an API request using Axios
-    const baseUrl =
-      "https://pixabay.com/api/?key=25540812-faf2b76d586c1787d2dd02736";
+    const baseUrl = `https://pixabay.com/api/?key=25540812-faf2b76d586c1787d2dd02736&per_page=9&page=1`;
 
     const url = req.query.type ? `${baseUrl}&q=${req.query.type}` : baseUrl;
     const response: AxiosResponse = await axios.get(url);
@@ -14,6 +14,45 @@ router.get("/images", async (req: Request, res: Response) => {
     const data = response.data.hits;
     // Send the response
     res.json(data);
+  } catch (error) {
+    // Handle error
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+const sortArrayById = (data: Image[]): Image[] => {
+  return data.sort((a, b) => a.id - b.id);
+};
+
+router.get("/sort", async (req: Request, res: Response) => {
+  try {
+    // Make an API request using Axios
+    const baseUrl =
+      "https://pixabay.com/api/?key=25540812-faf2b76d586c1787d2dd02736";
+
+    const url = req.query.type ? `${baseUrl}&q=${req.query.type}` : baseUrl;
+    const response: AxiosResponse = await axios.get(url, {
+      transformResponse: [
+        (responseData) => sortArrayById(JSON.parse(responseData).hits),
+      ],
+    });
+    // Send the response
+    res.json(response.data);
+  } catch (error) {
+    // Handle error
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+router.get("/paginate", async (req: Request, res: Response) => {
+  try {
+    // Make an API request using Axios
+    const baseUrl = `https://pixabay.com/api/?key=25540812-faf2b76d586c1787d2dd02736&per_page=9&page=${req.query.page}`;
+
+    const url = req.query.type ? `${baseUrl}&q=${req.query.type}` : baseUrl;
+    const response: AxiosResponse = await axios.get(url);
+    // Send the response
+    res.json(response.data);
   } catch (error) {
     // Handle error
     res.status(500).json({ error: "An error occurred" });
